da207e1cf158a04dab50a53530f02d9015663866
0xJeremy <jeremy.kanovsky@tufts.edu>
2020-03-30 18:17:26 -0400

    Began work experimentally with Kepler.gl

---
+++ scripts/process_data.py
@@ -0,0 +1,19 @@
+import csv
+
+input_file = 'input.csv'
+output_file = 'output.csv'
+cols_to_remove = [9]
+
+cols_to_remove = sorted(cols_to_remove, reverse=True)
+row_count = 0
+
+with open(input_file, "r") as source:
+    reader = csv.reader(source)
+    with open(output_file, "w", newline='') as result:
+        writer = csv.writer(result)
+        for row in reader:
+            row_count += 1
+            print('\r{0}'.format(row_count), end='')
+            for col_index in cols_to_remove:
+                del row[col_index]
+            writer.writerow(row)
