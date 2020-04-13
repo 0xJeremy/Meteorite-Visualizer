740e8dd27fbdbe7559b70fa54436cd7582a71bde
benlondon99 <32783398+benlondon99@users.noreply.github.com>
2020-04-13 12:50:28 -0400

    Added Class Chart

--- scripts/process_data.py
+++ scripts/process_data.py
@@ -1,8 +1,8 @@
 import csv
 
 input_file = 'input.csv'
-output_file = 'output.csv'
-cols_to_remove = [9]
+output_file = 'output_1.csv'
+cols_to_remove = [5,9]
 
 cols_to_remove = sorted(cols_to_remove, reverse=True)
 row_count = 0
---
+++ src/src/components/ClassChart.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import { scaleBand, scaleLinear } from 'd3-scale';
+import { max } from 'd3-array';
+import { axisBottom, axisLeft } from 'd3-axis';
+import { select } from 'd3-selection';
+import { histogram } from 'd3-array';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+  },
+  paper: {
+    padding: theme.spacing(1),
+    marginBottom: '8px',
+    marginRight: '8px',
+    marginTop: '16px',
+    textAlign: 'center',
+    color: '#4fbbd6',
+    fontSize: '40px',
+    backgroundColor: '#242730',
+    minHeight: '31vh',
+    padding: '0 0 0 0'
+  },
+  svg: {
+    minHeight: '30vh',
+    minWidth: '100%',
+  },
+  text: {
+    textAnchor: "end",
+    color: '#4fbbd6',
+    fontSize: '25px'
+  },
+}));
+
+
+export default function MassChart(props) {
+  const classes = useStyles();
+  const data = props.data;
+  const hoverItem = props.hoverItem;
+
+  function vw(view_width) {
+    return view_width * (window.innerWidth / 100)
+  }
+
+  function vh(view_height) {
+    return view_height * (window.innerHeight / 100)
+  }
+
+  const svgWidth = vw(23),
+        svgHeight = vh(24);
+
+  const margin = { top: vh(1), right: 0, bottom: 0, left: vw(5) },
+         width = svgWidth - margin.left - margin.right,
+        height = svgHeight - margin.top - margin.bottom;
+
+
+  return (
+    <Paper className={classes.paper}>
+      <svg className={classes.svg}>
+        <g transform={`translate(${margin.left}, ${margin.top})`}>
+          <g transform={`translate(0, ${height})`} ref={node => select(node).call(axisBottom(x))} />
+          <g>
+            <g ref={node => select(node).call()}/> 
+            <text className={classes.text} transform="rotate(-90)" y={-vw(2)-2} x={-svgHeight/4} style={{fill: '#4fbbd6'}}>
+
+              # Meteorites
+            </text>
+          </g>
+
+          <text className={classes.text} y={vh(28)+2} x={svgWidth/2+vw(1)} style={{fill: '#4fbbd6'}}>
+              Class/Type
+
+          </text>
+        </g>
+      </svg>
+    </Paper>
+  )
+};
