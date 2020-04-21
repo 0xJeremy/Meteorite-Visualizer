a74303725e048d36966dbab5ffe8c873badb2a72
benlondon99 <32783398+benlondon99@users.noreply.github.com>
2020-04-20 23:22:49 -0400

    Rounded data to thousandths

--- scripts/json_process_data.py
+++ scripts/json_process_data.py
@@ -15,7 +15,7 @@ with open(input_file, "r") as source:
                 row_count+=1
                 continue
 
-            if (len(row[3].split(' ')[0].split('/')) != 3 or row[4] == '' or row[5] == '' or row[2]==''
+            if (len(row[3].split(' ')[0].split('/')) != 3 or row[4] == '' or row[5] == '' or row[2]=='' or row[2]=='0'
                 or int(row[3].split(' ')[0].split('/')[2]) < 1000):
                 continue
 
@@ -27,7 +27,7 @@ with open(input_file, "r") as source:
             result.write(("\"coordinates\":[{},{}],").format(row[4],row[5]))
             result.write(("\"name\":\"{}\",").format(row[0]))
             result.write(("\"class\":\"{}\",").format(row[1]))
-            result.write(("\"mass\":\"{}\",").format(row[2]))
+            result.write(("\"mass\":\"{}\",").format(str(round(float(row[2]), 3))))
             result.write(("\"year\":{}").format(row[3].split(' ')[0].split('/')[2]))
             result.write("}")
         result.write("]")
